5394cf65f0a1c676daef5c9fdf1507834745a388
“pidtilok” <pidtilok23alex@gmail.com>
2023-01-28 19:01:09 +0200

    23

---
+++ script.js
@@ -0,0 +1,7 @@
+const year = +prompt("Your year of birth?");
+const yearNow = 2023;
+const age = yearNow - year;
+const name = prompt("Your name?");
+const surname = prompt("Your last name?");
+alert("User Bio: " + name + " " + surname + " " + age + " years old ")
+
