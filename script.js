1ae8b3511679fe2783a40cc284701621c21d3efa
“pidtilok” <pidtilok23alex@gmail.com>
2023-02-01 18:58:41 +0200

    23

--- script.js
+++ script.js
@@ -8,49 +8,49 @@ const age = yearNow - year;
 
 let leaps
 if (year % 400 == 0 || year % 4 == 0) {
-  console.log(leaps = '(leap year),')
+  leaps = '(leap year)'
 }
 else {
-  console.log(leaps = '(not a leap year)')
+  leaps = '(not a leap year)'
 }
 
 
 let zodiac
 if (month == 1 && date >= 20 || month == 2 && date <= 18) {
-  console.log(zodiac = 'Aquaris')
+  zodiac = 'Aquaris'
 }
 else if (month == 2 && date >= 19 || month == 3 && date <= 20) {
-  console.log(zodiac = 'Pisces')
+  zodiac = 'Pisces'
 }
 else if (month == 3 && date >= 21 || month == 4 && date <= 19) {
-  console.log(zodiac = 'Aries')
+  zodiac = 'Aries'
 }
 else if (month == 4 && date >= 20 || month == 5 && date <= 20) {
-  console.log(zodiac = 'Taurus')
+  zodiac = 'Taurus'
 }
 else if (month == 5 && date >= 21 || month == 6 && date <= 20) {
-  console.log(zodiac = 'Gemini')
+  zodiac = 'Gemini'
 }
 else if (month == 6 && date >= 21 || month == 7 && date <= 22) {
-  console.log(zodiac = 'Cancer')
+  zodiac = 'Cancer'
 }
 else if (month == 7 && date >= 23 || month == 8 && date <= 22) {
-  console.log(zodiac = 'Leo')
+  zodiac = 'Leo'
 }
 else if (month == 8 && date >= 23 || month == 9 && date <= 22) {
-  console.log(zodiac = 'Virgo')
+  zodiac = 'Virgo'
 }
 else if (month == 9 && date >= 23 || month == 10 && date <= 22) {
-  console.log(zodiac = 'Libra')
+  zodiac = 'Libra'
 }
 else if (month == 10 && date >= 23 || month == 11 && date <= 21) {
-  console.log(zodiac = 'Scorpio')
+  zodiac = 'Scorpio'
 }
 else if (month == 11 && date >= 22 || month == 12 && date <= 21) {
-  console.log(zodiac = 'Sagittarius')
+  zodiac = 'Sagittarius'
 }
 else if (month == 12 && date >= 22 || month == 1 && date <= 19) {
-  console.log(zodiac = 'Capricorn')
+  zodiac = 'Capricorn'
 }
 document.write(`User Bio: ${name} ${surname}, ${age} years old, ${zodiac}, ${leaps}`);
 
