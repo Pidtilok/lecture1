d47da2ee731854ff16bc9c9a1c568b1ac476a405
“pidtilok” <pidtilok23alex@gmail.com>
2023-02-08 20:54:41 +0200

    23

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let arr = [];
 let length = +prompt('Enter the lenght of the array')
+let res = 1
+
 
 while (length < 2 || length > 10) {
-  continue;
+  length = +prompt('The lenght of the array must be at least 2 and no more than 10')
+
+  if (length < -2 && length > -10) {
+    length = Math.abs(length)
+  }
+  lenght = Math.round(length)
 }
+for (let i = 0; i < length; i++) {
+  arr.push(Math.round(Math.random()*10))
+}
+console.log(arr);
+
+for (let el of arr) {
+  res = res * el
+}
+console.log(res)
